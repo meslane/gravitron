a05b9ffd6334cba0fa4078179f1b903ffa213caf
62fe50 <merrickslane@gmail.com>
2021-02-25 16:11:45 -0800

    changed to pygame for graphics and added colors

--- gravitron.py
+++ gravitron.py
@@ -1,25 +1,27 @@
 import math
 from os import system
 import time
+import pygame
 
 #units are metric (kg, meters, m/sec, newtons)
 
 class body:
-    def __init__(self, mass, position, velocity, char):
+    def __init__(self, mass, position, velocity, color, size):
         self.mass = mass
         self.position = position #position coords
         
         self.forces = list() #list of force vectors on object
         self.accel = (0,0) #instantanious acceleration vector
         self.velocity = velocity
-        self.char = char
+        self.color = color
+        self.size = size #pixel radius
         
     def getMass(self):
         return self.mass
         
     def getPos(self):
         return self.position
-        
+      
     def appendForce(self, obj):
         self.forces.append((fg(self, obj), angle(self, obj)))
         
@@ -91,17 +93,29 @@ def addRectVectors(v1, v2):
     return (v1[0] + v2[0], v1[1] + v2[1])
 
 def main():
+    RES_X = 1280
+    RES_Y = 720
+    SCALE1 = 10e8
+    SCALE2 = 2
+    screen = pygame.display.set_mode([RES_X,RES_Y])
+
     bodylist = []
     
-    bodylist.append(body(2e30, (0,0), (0,0), '*')) #sun
-    bodylist.append(body(6e24, (150e9,0), (30000, 1.5708), 'e')) #earth
-    bodylist.append(body(4.87e24, (0, 109e9), (35000, 3.14195), 'v')) #venus
-    bodylist.append(body(3.29e23, (64e9, 0), (48000, 1.5708), 'm')) #mercury
-    #bodylist.append(body(10e27, (180e9, 0), (10000, 1.5708), 'r'))
+    bodylist.append(body(2e30, (0,0), (0,0), (255,150,0), 25)) #sun
+    bodylist.append(body(6.39e23, (0, 237.71e9), (24060, 3.14159), (255,69,0), 8)) #mars
+    bodylist.append(body(6e24, (150e9,0), (30000, 1.5708), (0,0,255), 10)) #earth
+    bodylist.append(body(4.87e24, (0, 109e9), (35000, 3.14195), (255,255,0), 10)) #venus
+    bodylist.append(body(3.29e23, (64e9, 0), (48000, 1.5708), (128,128,128), 7)) #mercury
+    #bodylist.append(body(10e27, (180e9, 0), (10000, 1.5708), (255,0,255), 15))
     
     tick = 1000
     t = 0
-    while True:
+    loop = True
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+        
         for b1 in bodylist:
             for b2 in bodylist:
                 if (b1 != b2):
@@ -111,18 +125,11 @@ def main():
             b.updateData(tick)
        
         if (t % 86400 == 0):
-            system("cls")
-            grid = [[' ' for x in range(50)] for y in range(50)]
-            for x in range(50):
-                for y in range(50):
-                    for b in bodylist:
-                        if ((int(b.getPos()[0] / 10e9) + 25 == x and int(b.getPos()[1] / 10e9) + 25 == y)):
-                            grid[y][x] = b.symbol()
-                    print(grid[y][x], end = ' ')
-                print()
-                
-            print("t={}\n".format(t))
+            screen.fill((0,0,0))
+            for b in bodylist:
+                pygame.draw.circle(screen, b.color, (int(b.getPos()[0] / SCALE1) + int(RES_X/2), int(b.getPos()[1] / SCALE1) + int(RES_Y/2)), int(b.size/SCALE2))
         
+            pygame.display.flip()
         
         t += tick
     
